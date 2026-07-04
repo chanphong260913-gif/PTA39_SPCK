132867a66c7050f94c0d32eb30febcb1fad93b9c
chanphong260913-gif <chanphong260913@gmail.com>
2026-07-04 21:27:36 +0700

    tralalero tra la la

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-1+1
 from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 from PyQt6 import uic
-import os
+import os 
+
+
 
 from pages.login import LoginPage  # trang dau tien truy cap
 from pages.home import HomePage
@@ -14,6 +15,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # chi chay khi run bang app.py
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    #first_page = LoginPage(main_window=None, root_dir=BASE_DIR)
-    first_page = HomePage(main_window=None, root_dir=BASE_DIR, cur_acc={ "email": "abc@gmail.com", "password": "123456"})
+    first_page = LoginPage(main_window=None, root_dir=BASE_DIR)
+    #first_page = HomePage(main_window=None, root_dir=BASE_DIR, cur_acc={ "email": "abc@gmail.com", "password": "123456"})
     sys.exit(app.exec())
